0d49cc1a78220d11b397c95fa6c364e038cd33c5
Gam13 <joaovictorgam13@gmail.com>
2024-11-25 11:45:01 -0300

    first commit

---
+++ include/Code Framework/engine.hpp
@@ -0,0 +1,28 @@
+#ifndef ENGINE_H
+#define ENGINE_H
+
+#include "SDL.h"
+#include <iostream>
+#include <string>
+
+class Engine {
+private:
+    std::string gameName;
+    int screenWidth;
+    int screenHeight;
+    SDL_Renderer* gameRenderer = nullptr;
+    SDL_Window* gameWindow = nullptr;    
+    bool gameRunning = false;            
+
+public:
+    Engine(const std::string& title, int width, int height);
+    ~Engine();
+
+    void initialize();
+    void renderer();
+    void handleEvents();
+    void clean();
+    bool isRunning() const; // `const` para indicar que não modifica o estado do objeto
+};
+
+#endif
---
+++ src/engine.cpp
@@ -0,0 +1,69 @@
+#include "engine.hpp"
+
+Engine::Engine(const std::string& title, int width, int height)
+    : gameName(title), screenWidth(width), screenHeight(height) {
+
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        std::cerr << "O SDL não pôde ser iniciado: " << SDL_GetError() << std::endl;
+        gameRunning = false;
+    } else {
+        gameRunning = true;
+    }
+}
+
+Engine::~Engine() {
+    clean();
+    SDL_Quit();
+}
+
+void Engine::initialize() {
+
+    gameWindow = SDL_CreateWindow(
+        gameName.c_str(),
+        SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+        screenWidth, screenHeight,
+        SDL_WINDOW_SHOWN
+    );
+
+    if (gameWindow == nullptr) {
+        std::cerr << "Janela do SDL não pôde ser criada: " << SDL_GetError() << std::endl;
+        gameRunning = false;
+        return;
+    }
+
+    gameRenderer = SDL_CreateRenderer(gameWindow, -1, SDL_RENDERER_ACCELERATED);
+    if (gameRenderer == nullptr) {
+        std::cerr << "Renderer do SDL não pôde ser criado: " << SDL_GetError() << std::endl;
+        gameRunning = false;
+    }
+}
+
+void Engine::handleEvents() {
+    SDL_Event e;
+    while (SDL_PollEvent(&e) != 0) {
+        if (e.type == SDL_QUIT) {
+            gameRunning = false;
+        }
+    }
+}
+
+void Engine::renderer() {
+    SDL_SetRenderDrawColor(gameRenderer, 100, 42, 100, 255);
+    SDL_RenderClear(gameRenderer);
+    SDL_RenderPresent(gameRenderer);
+}
+
+void Engine::clean() {
+    if (gameRenderer != nullptr) {
+        SDL_DestroyRenderer(gameRenderer);
+        gameRenderer = nullptr;
+    }
+    if (gameWindow != nullptr) {
+        SDL_DestroyWindow(gameWindow);
+        gameWindow = nullptr;
+    }
+}
+
+bool Engine::isRunning() const {
+    return gameRunning;
+}
